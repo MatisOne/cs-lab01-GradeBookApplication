50626ca63da84449de1e970717ba33eb0f541940
Mateusz Sarna <16354548+MatisOne@users.noreply.github.com>
2022-04-03 06:02:14 +0200

    completed rankedgradebook class and all methods

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,43 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("You need at least 5 students");
+            var a = (int)Math.Ceiling(Students.Count * 0.2);
+            var gradeList = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (gradeList[a - 1] <= averageGrade)
+                return 'A';
+            else if (gradeList[(a * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (gradeList[(a * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (gradeList[(a * 4) - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
